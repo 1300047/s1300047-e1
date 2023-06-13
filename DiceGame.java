139791bfa0d5fd3ac338d590bf0b328f31ba12d3
1300047 <136040541+1300047@users.noreply.github.com>
2023-06-13 09:45:11 +0900

    Greetings added

--- DiceGame.java
+++ DiceGame.java
@@ -1,14 +1,27 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class DiceGame{
     public static void main(String[] args) {
         Dice dice = new Dice();
+        Greeting greet = new Greeting();
 
         dice.RoleDice();
    }
-
 }
 
+class Greeting{
+    String name;
+
+    Greeting(){
+        System.out.println("What is your name?");
+        Scanner sc = new Scanner(System.in);
+
+        name = sc.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
 
 class Dice{
     int sum;
